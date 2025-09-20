d37fe1369adacc0db9f86e1c77b8f1781e73de9d
IknuDeer <ethanhannum10@gmail.com>
2025-09-20 13:31:55 -0700

    Make reverseString function reverse given string
    String is reverse by have a for loop that starts at strings length - 1,
    that adds the given letter starting at the end of a string
    to finalString. Then iterated through all letters return finalString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // let finalString = ""
+    let finalString = ""
+    // for loop letting i = string.length - 1, while i >=0, i--
+    for (let i = string.length - 1; i >= 0; i--) {
+        // finalString concats the letter at i of string
+        finalString = finalString.concat(string[i]);
+    }
+    return finalString;
 };
 
 // Do not edit below this line
