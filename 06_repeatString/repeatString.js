8e48180f749102b100cf83f41b3cd9e589fa51a6
IknuDeer <ethanhannum10@gmail.com>
2025-09-20 12:34:44 -0700

    Write repeatString function
    repeatString will take provided string and repeat it as many times
    as given to num using simple for loop and adding repeats together

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let final = ""
+    for (i = 1; i <= num; i++) {
+        final = final.concat(string)
+    }
+    return final
 };
 
 // Do not edit below this line
