47a5bd017a263b27accd55a4df0c8fa50aa7438a
IknuDeer <ethanhannum10@gmail.com>
2025-09-21 14:55:23 -0700

    Add comment explaining leapYears function

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,6 @@
 const leapYears = function(year) {
+    // If year is divisible by 4
+    // AND EITHER not divisible by 100 OR || divisible by 400 it is a leap year
     if ((year % 4 === 0) && ((year % 100 !== 0) || year % 400 === 0)) {
         return true
     } else {
