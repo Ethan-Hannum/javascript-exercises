8a819c2fc89287ed7bedba0b58f1eae5bb42d661
IknuDeer <ethanhannum10@gmail.com>
2025-09-21 14:01:13 -0700

    Write sumAll function and algorithm
    Take numOne and numTwo and put them in arr array. Then check if either
    numOne or numTwo is less than zero, or if it's a non-integer. If so,
    return ERROR. Next if else statement checks whether numOne or numTwo
    is the larger number, then set i to the smaller number + 1 and while
    i is less than bigger number push i to arr and add one to i. Finally,
    return the reduced value of arr.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let arr = [numOne, numTwo]
+    // Return an error if a number is negative, non integer, non number/array)
+    if ((numOne < 0) || (numTwo < 0)) return "ERROR";
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) return "ERROR";
+    /* If numOne bigger than numTwo, let i = numTwo + 1 and iterate over i,
+     pushing i into arr then adding one to i while i is less than numOne.
+     Else do the same but reverse numOne and numTwo*/
+    if (numOne > numTwo) {
+        let i = numTwo + 1;
+        while (i < numOne) {
+            arr.push(i);
+            i++
+        }
+    } else {
+        let i = numOne + 1;
+        while (i < numTwo) {
+            arr.push(i);
+            i++
+        }
+    }
+    // Return the sum of arr
+    return arr.reduce((sum, current) => sum + current, 0);
+    
 };
 
 // Do not edit below this line
