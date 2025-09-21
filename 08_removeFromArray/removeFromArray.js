d1a3f25cd006d5ac5c998b5c3c09beb4dd8e8c60
IknuDeer <ethanhannum10@gmail.com>
2025-09-21 12:20:02 -0700

    Add algorith to remove wanted items from array
    Put arr onto workingArray. Then filter through workingArray itrerating
    through item. If item IS NOT included in removal then filter it through.
    Then return filtered items

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removal) {
+    // Declare workingArr and initialize it to arr
+    let workingArr = arr.slice();
+    /* workingArr iterates through its items, and if the item IS NOT included in removal array... 
+    then filter it through. Then return all filtered items*/
+    return workingArr.filter((item) => !removal.includes(item));
+     
+     
 };
 
 // Do not edit below this line
