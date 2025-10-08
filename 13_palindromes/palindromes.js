8de42dbd48bbc2aadc3d4239afc9c11d2cb87994
IknuDeer <ethanhannum10@gmail.com>
2025-10-07 22:34:12 -0700

    Begin rewrite of palindrome function
    Previous function could not handle later tests. Current function is a
    work in progress.
    
    First the str parameter is split into array arr. Next, the punctuation
    is reduced out of the array and the index becomes a key with the
    punctuation character being saved as the value. Then array arr, with the
    punctuation removed, is reversed.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,8 +1,25 @@
 const palindromes = function (str) {
     // Split str at each letter, reverse the array, then join array together 
     // and assign it to arr
-    let arr = str.split("").reverse().join("");
-    return arr === str;
+    let arr = str.split("")
+
+    let punctuation = arr.reduce((obj, char, index) => {
+        if (char === (("!") || (".") || (",") || ("?") || (` `))) {
+            obj[index] = char;
+            arr.splice(index, 1);
+            return obj;
+        } else {
+            return obj;
+        }
+    }, {});
+
+    arr.reverse();
+
+    // Take punctuation out of arr and assign it to an object with the key being
+    // the index of punctuation and value being the punctuation itself
+    
+
+    return arr;
 };
 
 // Do not edit below this line
