0c8ffbf752be109a187cafc1e0bfbe437f9261c4
IknuDeer <ethanhannum10@gmail.com>
2025-10-07 23:37:38 -0700

    Finalize palindrome function rewrite
    Deleted the reverse method and wrote own that saves to punctuation.
    An empty array is assigned to finalArr that will be built as reversed
    original string array. A for loop will iterate over index,
    which is assigned to arrLength, reducing index by one each loop, until
    index is less than 0. arrLength is the length of arr at initialization.
    
    In the for loop if index is not equal to a punctuation key then unshift
    character at the index of arr into finalArr.
    Else unshift the punctuation character at the index of punctuation into
    finalArr.
    
    At the end of the function compare the original string to the joined
    reverse string of finalArr, return boolean result.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,8 +1,11 @@
 const palindromes = function (str) {
-    // Split str at each letter, reverse the array, then join array together 
-    // and assign it to arr
+    // Split str into arr and save the length of arr - 1 to arrLength
     let arr = str.split("")
+    const arrLength = arr.length - 1;
 
+    /* Create object punctuation that finds punctuation characters in arr and
+    saves the index as a key with the character being it's value. Then splices
+    the character out of arr.*/
     let punctuation = arr.reduce((obj, char, index) => {
         if (char === (("!") || (".") || (",") || ("?") || (` `))) {
             obj[index] = char;
@@ -13,9 +16,21 @@ const palindromes = function (str) {
         }
     }, {});
 
-    arr.reverse();
+    let finalArr = [];
+    // Take arrLength and use that to build finalArr as a reversed arr
+    // If index is not a punctuation key, unshift character at index of arr
+    // Else unshift punctuation character at index of punctuation
+    for (index = arrLength; index >= 0; --index) {
+        if (punctuation[index] === undefined) {
+            finalArr.unshift(arr[index]);
+        } else {
+            finalArr.unshift(punctuation[index]);
+        }
+    }
 
-    return arr;
+    // Compare original string to finalArr joined into string
+    // Return true if palindrome; Return false if not palindrome
+    return str === finalArr.join("");
 };
 
 // Do not edit below this line
