32e1e59c3aac5fd4b7d3672929fe5e6758d4c8d4
IknuDeer <ethanhannum10@gmail.com>
2025-10-07 17:29:11 -0700

    Refactor palindrome for readability
    Instead of declaring and assigning all those variables like before,
    now all the array methods are done and assigned to arr. Then arr is
    checked if it and str are equal.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,11 +1,8 @@
 const palindromes = function (str) {
-    // Split str at each letter and assign it to arr
-    let arr = str.split("");
-    // Reverse and join arr
-    arr.reverse();
-    let joinedArr = arr.join("");
-    // Return checking if arr === str;
-    return joinedArr === str;
+    // Split str at each letter, reverse the array, then join array together 
+    // and assign it to arr
+    let arr = str.split("").reverse().join("");
+    return arr === str;
 };
 
 // Do not edit below this line
