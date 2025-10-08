b0ca47c5856f64fe6b4fddee45337c4bba8916e9
IknuDeer <ethanhannum10@gmail.com>
2025-10-07 17:23:32 -0700

    Add code to function checking if str is palindrome
    First the function splits str into arr. Then arr uses the reverse method
    and is joined to joinedArr. Return the booleon result of checking if
    joinedArr is equal to str.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Split str at each letter and assign it to arr
+    let arr = str.split("");
+    // Reverse and join arr
+    arr.reverse();
+    let joinedArr = arr.join("");
+    // Return checking if arr === str;
+    return joinedArr === str;
 };
 
 // Do not edit below this line
