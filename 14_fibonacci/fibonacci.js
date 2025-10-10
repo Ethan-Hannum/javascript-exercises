7b47e31c4eb66131755ff8940e04f1c63d4116b4
IknuDeer <ethanhannum10@gmail.com>
2025-10-09 19:45:12 -0700

    Write function to return desired sequence result
    The object sequence is created to store sequence information. That data
    is looped through what is the fibonacci sequence the amount of interval.
    Loop starts at the 2nd sequence interation. Return the current sequence
    number.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(interval) {
+    // The fibonacci sequence as data. 
+    // Far is the integer to be replaced
+    // Previous is the previous integer done in the fibonacci sequence
+    // Current is the most recent integer in the fibonacci sequence
+    // The value in current will replace the value in previous
+    const sequence = {
+        far: 0,
+        previous: 1,
+        current: 1
+    };
+
+    // Fibonacci loop
+    // Start loop at second fibonacci sequence and loop until at desired interval
+    for (i = 2; i <= interval; ++i) {
+        sequence.current = sequence.far + sequence.previous;
+        sequence.far = sequence.previous;
+        sequence.previous = sequence.current;
+    }
+    
+    return sequence.current;
 
 };
 
