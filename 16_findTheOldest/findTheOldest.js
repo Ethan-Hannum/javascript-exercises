e9491bc2388dad508cd52ffa4637bd1d0bedac20
IknuDeer <ethanhannum10@gmail.com>
2025-10-12 13:08:13 -0700

    Write function to find oldest person in array
    First, the date is saved to Date. A forEach method goes through the arr
    array, which is the given array, and if the person object doesn't have
    a yearOfDeath key it is given one with todays year as it's value.
    
    Next the array is sorted through to have the oldest be put last in the
    array. Finally, pop the last object in the array to get the name for the
    test.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const date = new Date();
 
+    // If yearOfDeath is undefined on person, set it to todays date
+    arr.forEach(person => {
+        if (person.yearOfDeath === undefined) person.yearOfDeath = date.getFullYear();
+    });
+
+    let sorted = arr.sort((itemA, itemB) => {
+        // yearOfDeath - yearOfBirth is age of person. Sorts array based of age
+        if (itemA.yearOfDeath - itemA.yearOfBirth > itemB.yearOfDeath - itemB.yearOfBirth) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    return sorted.pop();
 };
 
 // Do not edit below this line
